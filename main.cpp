6fd2c78cae6912f3fe7ca0132f5a4886b37aab47
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2020-06-02 15:36:59 +0200

    remove Random for consistency in cuda tests

--- main.cpp
+++ main.cpp
@@ -10,17 +10,24 @@ int main()
 {
     int n;
 
-    cout << "Size of the matrix? " << endl;
+    cout << "Size of the matrix (>2)? " << endl;
     cin >> n;
 
-    auto m = MatrixXd::Random(n, n);
-    auto v = VectorXd::Random(n);
+    MatrixXd m(n,n);
+    m << VectorXd::Zero(n), MatrixXd::Identity(n, n).block(0, 0, n, n-1);
+    m = m + MatrixXd::Identity(n, n);
+    VectorXd v(n);
+    for (int ii = 0; ii < n-1; ++ii) {
+        v[ii] = 2*ii + 1;
+    }
+    v[n-1] = n-1;
+
 
     VectorXd s = m.partialPivLu().solve(v);
 
-    cout << "Random matrix:\n\n";
+    cout << "Matrix:\n\n";
     cout << m << endl << endl;
-    cout << "Random vector:\n\n";
+    cout << "Vector:\n\n";
     cout << v << endl << endl;
     cout << "Solution (x) of M*x = v:\n\n";
     cout << s << endl << endl;
